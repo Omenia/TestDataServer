f47933eac4380c9b60a0fef905b9e4188902be10
Juha Alhonen <juha.alhonen@siili.com>
2018-10-20 19:52:40 +0300

    FIxing flake errors

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ def read(fname):
 
 
 setup(
-    name = "ultimateTestDataServer",
-    version = "0.0.1",
-    author = "Siili Salabs",
-    author_email = "info@siili.com",
-    description = "Simple and powerful test data server",
-    license = "keksijotain",
-    keywords = "example documentation tutorial",
-    url = "http://packages.python.org/ultimateTestDataServer",
+    name="ultimateTestDataServer",
+    version="0.0.1",
+    author="Siili Salabs",
+    author_email="info@siili.com",
+    description="Simple and powerful test data server",
+    license="keksijotain",
+    keywords="example documentation tutorial",
+    url="http://packages.python.org/ultimateTestDataServer",
     packages=['source', 'tests'],
-    install_requires = ['connexion', 'Flask', 'flake8', 'pytest'],
+    install_requires=['connexion', 'Flask', 'flake8', 'pytest'],
     long_description=read('README'),
     classifiers=[
         "Development Status :: 3 - Alpha",
