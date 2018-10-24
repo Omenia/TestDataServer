b5e86daff0bd4bb02fca19daacc42e42a28322e6
Jari Winberg <44158878+JariWinberg@users.noreply.github.com>
2018-10-24 08:58:06 +0300

    issue-11: return valid test data, closes #11

--- tests/test_api.py
+++ tests/test_api.py
@@ -22,4 +22,8 @@ def test__status_endpoint(client):
 
 def test__testdata_endpoint(client):
     response = client.get('/testdata')
-    assert response.json == {"testdata": {"username": "user", "password": "pass"}}
+    assert response.json == {"testdata": {"key A1": "value A1", "key B1": "value B1", "key C1": "value C1"}}
+    response = client.get('/testdata')
+    assert response.json == {"testdata": {"key A2": "value A2", "key B2": "value B2", "key C2": "value C2"}}
+    response = client.get('/testdata')
+    assert response.json == {"testdata": {"key A1": "value A1", "key B1": "value B1", "key C1": "value C1"}}
