c9917587cad48cc86611a4a56c43612325e4c5a4
Jari Winberg <44158878+JariWinberg@users.noreply.github.com>
2019-02-03 20:06:59 +0200

    unittest output directory path fix

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-flake8 --output-file=output/flake8-output.log ../server
+flake8 --output-file=tests/output/flake8-output.log ../server
 
 export PYTHONPATH=../server
-pytest > output/pytest-output.log
+pytest > tests/output/pytest-output.log
